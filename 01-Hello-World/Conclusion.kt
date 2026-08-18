fbf316ed0bc55692cd26456731cc5bea1459980c
arianaagonzalez19-design <arianaagonzalez19@gmail.com>
2026-08-17 21:28:46 -0600

    Create Conclusion.kt

---
+++ 01-Hello-World/Conclusion.kt
@@ -0,0 +1,3 @@
+En conclusión, el primero de los temas que es Hello World nos permitió conocer la estructura básica de lo que es
+un programa en Kotlin y la forma en que podemos mostrar información en pantalla. Este ejercicio nos ayuda a comprender
+la sintaxis necesaria para comenzar a desarrollar los programas necesarios.
