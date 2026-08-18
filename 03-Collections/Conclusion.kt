cdc8b72418fdfa805cf7580d3462f2caf5c1be3c
arianaagonzalez19-design <arianaagonzalez19@gmail.com>
2026-08-17 21:32:19 -0600

    Create Conclusion.kt

---
+++ 03-Collections/Conclusion.kt
@@ -0,0 +1,3 @@
+En conclusión, el control de flujo permite determinar cómo y cuándo se ejecutan las diferentes instrucciones de un programa.
+Mediante condiciones y ciclos es posible tomar decisiones y repetir procesos, haciendo que los programas sean más dinámicos y capaces
+de responder a diferentes situaciones.
