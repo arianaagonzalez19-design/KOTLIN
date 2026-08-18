6983946804f3f9ac28df73e3c351d2349aee57f4
arianaagonzalez19-design <arianaagonzalez19@gmail.com>
2026-08-17 21:34:00 -0600

    Create Conclusion.kt

---
+++ 05-Functions/Conclusion.kt
@@ -0,0 +1,3 @@
+En conclusión, las funciones permiten organizar el código en bloques que realizan tareas
+específicas y que pueden reutilizarse cuando sea necesario. Su utilización ayuda a reducir la repetición
+de código y mejora la organización, legibilidad y mantenimiento de los programas.
