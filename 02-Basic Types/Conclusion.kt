3cce4379b1ed4bb0a788f9ea75a02f7680f62ce6
arianaagonzalez19-design <arianaagonzalez19@gmail.com>
2026-08-17 21:30:06 -0600

    Create Conclusion.kt

---
+++ 02-Basic Types/Conclusion.kt
@@ -0,0 +1,3 @@
+En conclusión, los tipos básicos de kotlin son fundamentales para trabajar con información dentro de la aplicacion.
+Permiten definir si un dato es un número, texto, carácter, valor booleano, entre otros y asi nos van facilitando 
+el manejo y almacenamiento correcto de los datos en un programa.
